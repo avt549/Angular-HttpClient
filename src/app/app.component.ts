00e2e5b1dd3e816a7c3abffdb67d1c7599de1175
avt549 <avt549@yandex.ru>
2023-04-13 10:43:11 +0300

    Получение данных в шаблон И создание объектов для Post запроса

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {HttpClient} from "@angular/common/http";
 
+export interface Todo{
+  completed: boolean
+  title:string
+  id?:number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'HttpClientAngulare';
+export class AppComponent implements OnInit {
+
+ public todos: Todo[] = []
+
+  todoTitle = ''
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    // Выполнение get запроса и вывод отовета в сонсоли
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(todos=>{console.log('Response :', todos)
+        this.todos=todos
+
+      })
+
+
+  }
+
+  addTodo() {
+   // Валидация на пустую строку
+   if(!this.todoTitle.trim()){
+     return
+   }
+// Если данные есть то мы кладём их в переменную newTodo
+   const newTodo: Todo = {
+     title: this.todoTitle,
+     completed: false
+   }
+
+//Выполняем метод пост который принимает в себя как минимум url и body
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos',newTodo)
+      .subscribe(todo=>{
+        console.log('todo :', todo)
+        //Добавляемм новый объект в массив todos
+        this.todos.push(todo)
+
+        //Присваиваем переменной пустую строку для тогочтобы очистить input
+        this.todoTitle=''
+    })
+
+
+  }
 }
+
